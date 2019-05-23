7bbb55ea9174db03d11387389e470e856f1d72a3
christian <christian.nascimento3@etec.sp.gov.br>
2019-05-22 21:13:19 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/menuappvoltar/Principal_activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menuappvoltar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.princinpal_layout);
+    }
+}
